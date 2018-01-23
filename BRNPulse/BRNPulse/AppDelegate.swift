c51a434760a2cc0f8ed9ba528524a5ee6da74ffc
Sandeep <sandeep.manala@achiever-105.local>
2018-01-23 13:49:45 +0530

    added login Screen

--- BRNPulse/BRNPulse/AppDelegate.swift
+++ BRNPulse/BRNPulse/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+    {
+        IQKeyboardManager.sharedManager().enable = true
+    
         // Override point for customization after application launch.
         return true
     }
